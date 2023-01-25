4669485f730fc188b4d26f90f216c405f48a2127
Nick Shockey <shockeynj@gmail.com>
2023-01-25 14:48:03 -0500

    added in_range function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn in_range(value: i32, min: i32, max: i32) -> bool {
+	let mut result = false;
+
+	if value >= min && value <= max {
+		result = true;
+	}
+
+	result
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn in_range_test() {
+		let result = in_range(10, 1, 15);
+		if !result {
+			panic!("Number in range test failed. Number not in range.")
+		}
+	}
 }

58c1eaa203685e66116d547dcd44e2eb6e906114
Nick Shockey <shockeynj@gmail.com>
2023-01-25 16:16:15 -0500

    Updated readme and docs

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
 # In range
 
-Return true if a number is in range.
+A Rust library to return true if a number is in range.
+
+## Usage
+
+First, install the library with
+
+```bash
+cargo add in_range
+```
+
+Then, in code:
+
+```rs
+use in_range::in_range;
+
+// Args: value, min, max
+let result = in_range(10, 1, 15);
+
+print!("{}", result);
+// Prints true;
+```
+
+You can also use `not_in_range` to return *false* when value is in range.
+
+```rs
+use in_range::not_in_range;
+
+// Args: value, min, max
+let result = not_in_range(25, 1, 15);
+
+print!("{}", result);
+// Prints true;
+```
 
 ## License
 
-In range uses the Rust standard MIT/Apache 2.0 dual license for best
+In range uses the Rust standard MIT/Apache-2.0 dual license for best
 compatibility. See
 [LICENSE-APACHE.txt](LICENSE-APACHE.txt) and [LICENSE-MIT.txt](LICENSE-MIT.txt)
 for the full licenses.
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,55 @@
+//! # In range
+//!
+//! Returns true if a number is in range.
+//!
+//! ## Usage
+//!
+//! First, install the library with
+//!
+//! ```bash
+//! cargo add in_range
+//! ```
+//!
+//! Then, in code:
+//!
+//! ```
+//! use in_range::in_range;
+//!
+//! // Args: value, min, max
+//! let result = in_range(10, 1, 15);
+//!
+//! print!("{}", result);
+//! // Prints true;
+//! ```
+//!
+//! You can also use `not_in_range` to return *false* when value is in range.
+//!
+//! ```
+//! use in_range::not_in_range;
+//!
+//! // Args: value, min, max
+//! let result = not_in_range(25, 1, 15);
+//!
+//! print!("{}", result);
+//! // Prints true;
+//! ```
+
+/// Return true if an i32 value is within range.
 pub fn in_range(value: i32, min: i32, max: i32) -> bool {
 	value >= min && value <= max
 }
 
+/// Return false if an i32 value is within range.
 pub fn not_in_range(value: i32, min: i32, max: i32) -> bool {
 	!in_range(value, min, max)
 }
 
+/// Return true if a u32 value is within range.
 pub fn in_range_u32(value: u32, min: u32, max: u32) -> bool {
 	value >= min && value <= max
 }
 
+/// Return false if a u32 value is within range.
 pub fn not_in_range_u32(value: u32, min: u32, max: u32) -> bool {
 	!in_range_u32(value, min, max)
 }

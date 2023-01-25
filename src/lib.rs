835693d838541e180903e9e42b5d752e0eb0d340
Nick Shockey <shockeynj@gmail.com>
2023-01-25 15:51:26 -0500

    added u32 functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@ pub fn not_in_range(value: i32, min: i32, max: i32) -> bool {
 	!in_range(value, min, max)
 }
 
+pub fn in_range_u32(value: u32, min: u32, max: u32) -> bool {
+	value >= min && value <= max
+}
+
+pub fn not_in_range_u32(value: u32, min: u32, max: u32) -> bool {
+	!in_range_u32(value, min, max)
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -21,6 +29,22 @@ mod tests {
 	#[test]
 	fn not_in_range_test() {
 		let result = not_in_range(100, 1, 15);
+		if !result {
+			panic!("Number not in range test failed. Number is in range.")
+		}
+	}
+
+	#[test]
+	fn in_range_u32_test() {
+		let result = in_range_u32(10, 1, 15);
+		if !result {
+			panic!("Number in range test failed. Number not in range.")
+		}
+	}
+
+	#[test]
+	fn not_in_range_u32_test() {
+		let result = not_in_range_u32(100, 1, 15);
 		if !result {
 			panic!("Number not in range test failed. Number is in range.")
 		}
